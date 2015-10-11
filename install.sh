bdfb0ae0ad47faddec76c5da3595c6fcdde67d75
Parker Snell <parkovski@users.noreply.github.com>
2015-10-11 14:39:37 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@ cp vimrc ~/.vimrc
 cp tmux-conf ~/.tmux.conf
 mkdir -p ~/.vim/colors
 cp github.vim ~/.vim/colors
+
+# install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
