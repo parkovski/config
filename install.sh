c4c8f1b0daa4ebd460aafac4c6fb59b6b19fb0dc
Parker Snell <parkovski@users.noreply.github.com>
2015-10-29 00:01:24 -0700

    add some vim plugins

--- install.sh
+++ install.sh
@@ -7,3 +7,9 @@ cp github.vim ~/.vim/colors
 # install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+git clone https://github.com/scrooloose/syntastic.git ~/.vim/bundle/syntastic
+git clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript
+#omnisharp vim looks interesting
+git clone https://github.com/fsharp/vim-fsharp.git ~/.vim/bundle/vim-fsharp
+echo "to install fsharp syntax checker, go to ~/.vim/bundle/vim-fsharp and run make"
