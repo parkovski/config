f113ed8ece971eca987e8e0346f8d9d6cb9b480a
Parker Snell <parkersnell@gmail.com>
2016-06-08 16:36:55 -0700

    update

--- gitcommands.sh.ps1
+++ gitcommands.sh.ps1
@@ -1,1 +1,7 @@
+git clone https://github.com/scrooloose/syntastic.git $HOME/.vim/bundle/syntastic
+git clone https://github.com/pangloss/vim-javascript.git $HOME/.vim/bundle/vim-javascript
+git clone --depth=1 https://github.com/rust-lang/rust.vim.git $HOME/.vim/bundle/rust.vim
 #omnisharp vim looks interesting
+#git clone https://github.com/fsharp/vim-fsharp.git $HOME/.vim/bundle/vim-fsharp
+#echo "to install fsharp syntax checker, go to $HOME/.vim/bundle/vim-fsharp and run make"
+git clone git://github.com/altercation/vim-colors-solarized.git $HOME/.vim/bundle/vim-colors-solarized
