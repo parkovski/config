1ae48c43d35888f08b8c1ce4da8f52984ac1ad36
Parker Snell <parkersnell@gmail.com>
2017-12-17 13:52:35 -0700

    symlink everything

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
-cp ./.gitconfig ~/.gitconfig
-cp ./.vimrc ~/.vimrc
-cp ./.gvimrc ~/.gvimrc
-ln -s $GH/config/.tmux.conf ~/.tmux.conf
-unzip ~/OneDrive/Documents/Utils/vimfiles.zip -d ~
-mv ~/vimfiles ~/.vim
+CONFIG=$(pwd)
+function linkf() {
+  rm "~/$1" 2>/dev/null
+  ln -s "$CONFIG/$1" "~/$1"
+}
+
+linkf .zshrc
+linkf .gitconfig
+linkf .vimrc
+linkf .gvimrc
+linkf .tmux.conf
+if [[ -f ~/OneDrive/Documents/Utils/vimfiles.zip ]]; then
+  unzip ~/OneDrive/Documents/Utils/vimfiles.zip -d ~
+  mv ~/vimfiles ~/.vim
+fi
 
