fa1041df8ed111b003c141ec2a56b00464f5e754
Parker Snell <parkersnell@gmail.com>
2018-11-10 01:51:03 +0100

    foo

--- install.ps1
+++ install.ps1
@@ -17,10 +17,17 @@ link $home\_gvimrc $pwd\.gvimrc
 link $home\.gvimrc $pwd\.gvimrc
 link $home\_vimrc $pwd\.vimrc
 link $home\.vimrc $pwd\.vimrc
-if (-not test-path $home\Documents\WindowsPowerShell) {
+
+if (-not (test-path "$home\Documents\WindowsPowerShell")) {
   mkdir $home\Documents\WindowsPowerShell
 }
-link $profile $pwd\profile.ps1
+link $home\Documents\WindowsPowerShell\profile.ps1 $pwd\profile.ps1
+
+if (-not (test-path "$home\Documents\PowerShell")) {
+  mkdir $home\Documents\PowerShell
+}
+link $home\Documents\PowerShell\profile.ps1 $pwd\profile.ps1
+
 link $home\shared $pwd\shared
 
 $userenv = [System.Environment]::GetEnvironmentVariable("Path", "User")
@@ -28,7 +35,7 @@ if ($userenv -inotcontains "$Home\shared\bin") {
   [System.Environment]::SetEnvironmentVariable("Path", "$Home\shared\bin;$userenv", "User")
 }
 
-if (-not test-path Env:\VCPKG_DEFAULT_TRIPLET) {
+if (-not (test-path Env:\VCPKG_DEFAULT_TRIPLET)) {
   [System.Environment]::SetEnvironmentVariable('VCPKG_DEFAULT_TRIPLET', 'x64-windows', 'User')
 }
 
