a2a199a8bd385384b1b2e2d9e0eb4d6f264aa327
Parker Snell <parkersnell@gmail.com>
2021-07-19 12:24:19 -0700

    android detection

--- shared/lib/sh/os.sh
+++ shared/lib/sh/os.sh
@@ -1,16 +1,18 @@
 OS_BASE=$(uname -s)
-if [[ "$OS_BASE" == "Darwin" ]]; then
-  OS_BASE="macOS"
+if [[ $OS_BASE == Darwin ]]; then
+  OS_BASE=macOS
 fi
 if [[ -f /etc/os-release ]]; then
   OS=$(cat /etc/os-release | sed -n "s/^NAME=\\\"\\(.\\+\\)\\\"/\\1/p")
+elif [[ $(uname -o) == Android ]]; then
+  OS=Android
 else
   OS=$OS_BASE
 fi
 export OS_BASE
 export OS
 
-if [[ "$OS_BASE" -eq "Linux" ]]; then
-  cat /proc/version | grep -iq Microsoft
+if [[ $OS_BASE == Linux ]]; then
+  cat /proc/version 2>/dev/null | grep -iq Microsoft
   export IS_WSL=$[! $?]
 fi
